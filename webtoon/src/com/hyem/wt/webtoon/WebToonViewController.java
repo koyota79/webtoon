4201af9d262b66c214ea108071cfe8c245542bf4
koyota79 <koyota798@gmail.com>
2022-07-03 16:05:44 +0900

    top,상세보기 추가

--- webtoon/src/com/hyem/wt/webtoon/WebToonViewController.java
+++ webtoon/src/com/hyem/wt/webtoon/WebToonViewController.java
@@ -9,18 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hyem.wt.common.AES256Chiper;
 import com.hyem.wt.lastview.service.LastViewItemService;
+import com.hyem.wt.main.service.MainPageService;
+import com.hyem.wt.webtoon.service.WebtoonItemService;
 
 @Controller
 public class WebToonViewController {
 
-	@RequestMapping("/webToonDetail")
-	public String LastViewItem(Model model, HttpServletRequest request) throws Exception{
-		ModelAndView mav = new ModelAndView();
-		return "/webtoon/webToonDetail";  
+	@Resource(name="WebtoonItemService")
+	private WebtoonItemService service;
+	
+	
+	@RequestMapping(value="/webToonDetail.do" , method = { RequestMethod.POST})
+	public ModelAndView LastViewItem(@RequestParam HashMap<String,Object> param, Model mode) throws Exception{
+		ModelAndView mv = new ModelAndView();
+		
+		HashMap hmap = service.selectWebtoonItem("selectWebtoonItemData" , param);
+		//System.out.println("::::::LastViewItemList Size::::::"+list.size());
+	    
+	    mv.addObject("DATA", hmap);
+	    mv.setViewName("/webtoon/webToonDetail");
+	    return mv;
 	}
 }
