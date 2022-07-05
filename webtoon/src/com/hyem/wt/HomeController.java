61cf3391cd76f91a95465a5370cb3cf664f0cdab
injae <injae8317@naver.com>
2022-07-05 23:08:49 +0900

    injae 토큰 만듬

--- webtoon/src/com/hyem/wt/HomeController.java
+++ webtoon/src/com/hyem/wt/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
+ * injae 토큰
  */
 @Controller
 public class HomeController {
